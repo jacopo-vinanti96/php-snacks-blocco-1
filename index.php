0b59f9aa736be525f0c789e5170b22c7c4251545
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-04 12:39:22 +0200

    Snack 1

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+// Mostra tutti gli errori/notice/warning
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$partite = [
+  [
+    'casa' => [
+      'squadra' => 'Olimpia',
+      'punteggio' => 78
+    ],
+    'ospite' => [
+      'squadra' => 'Gervasio',
+      'punteggio' => 68
+    ],
+  ],
+  [
+    'casa' => [
+      'squadra' => 'Atletica',
+      'punteggio' => 72
+    ],
+    'ospite' => [
+      'squadra' => 'Sportiva',
+      'punteggio' => 89
+    ],
+  ],
+]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/style.css">
+<title>Snacks</title>
+</head>
+<body>
+<main>
+  <p>
+    <?php
+    for ( $i = 0; $i < count($partite); $i++ ) {
+      echo $partite[$i]['casa']['squadra'].' - '.$partite[$i]['ospite']['squadra'].'<br>'.$partite[$i]['casa']['punteggio'].' - '.$partite[$i]['ospite']['punteggio'].'<br>';
+    }
+    ?>
+  </p>
+</main>
+
+</body>
+</html>
