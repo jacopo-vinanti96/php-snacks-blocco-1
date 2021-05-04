71e83ed4ccfde343a7728c7b99d4c528aaf7c725
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-04 12:41:18 +0200

    Snack 1 indentazione

--- index.php
+++ index.php
@@ -35,19 +35,21 @@ $partite = [
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="css/style.css">
 <title>Snacks</title>
 </head>
 <body>
-<main>
-  <p>
-    <?php
-    for ( $i = 0; $i < count($partite); $i++ ) {
-      echo $partite[$i]['casa']['squadra'].' - '.$partite[$i]['ospite']['squadra'].'<br>'.$partite[$i]['casa']['punteggio'].' - '.$partite[$i]['ospite']['punteggio'].'<br>';
-    }
-    ?>
-  </p>
-</main>
+  <main>
+    <p>
+      <?php
+        for ( $i = 0; $i < count($partite); $i++ ) {
+          echo $partite[$i]['casa']['squadra'].' - '
+          .$partite[$i]['ospite']['squadra'].'<br>'
+          .$partite[$i]['casa']['punteggio'].' - '
+          .$partite[$i]['ospite']['punteggio'].'<br>';
+        }
+      ?>
+    </p>
+  </main>
 
 </body>
 </html>
